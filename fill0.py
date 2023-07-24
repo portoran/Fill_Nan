c1fcb7c9620cc2ebd3763db8f0aa0a89eee0c75e
Ryan Adhitya Nugraha <114966562+portoran@users.noreply.github.com>
2023-07-24 13:05:34 +0700

    Add files via upload

--- fill0.py
+++ fill0.py
@@ -5,14 +5,15 @@ Created on Mon Jul 24 11:32:37 2023
 @author: ryan.nugraha
 """
 
+
 import pandas as pd
-import numpy as np
 import streamlit as st
 import io
 
-host=st.secrets.db_credentials.host,
-user=st.secrets.db_credentials.user,
-password=st.secrets.db_credentials.password,
+# Add your secrets
+host = st.secrets.db_credentials.host
+user = st.secrets.db_credentials.user
+password = st.secrets.db_credentials.password
     
 def fill_nan_with_zero(df):
     return df.fillna(0)
@@ -35,7 +36,7 @@ def main():
         df_filled.to_excel(output_buffer, index=False)
         st.download_button("Download filled DataFrame as Excel", data=output_buffer.getvalue(), file_name="output.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
-
 if __name__ == "__main__":
     main()
+
 
