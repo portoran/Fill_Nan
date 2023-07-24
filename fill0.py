20cd039663d29edcbe919e3172efcafbc7887d0a
Ryan Adhitya Nugraha <114966562+portoran@users.noreply.github.com>
2023-07-24 12:24:48 +0700

    Add files via upload

--- fill0.py
+++ fill0.py
@@ -10,18 +10,8 @@ import numpy as np
 import streamlit as st
 import io
 
-st.write("DB username:", st.secrets["DB_USERNAME"])
-st.write("DB password:", st.secrets["DB_PASSWORD"])
-st.write("KEY:", st.secrets["1234"])
-
-# And the root-level secrets are also accessible as environment variables:
-
-import os
-
-st.write(
-    "Has environment variables been set:",
-    os.environ["DB_USERNAME"] == st.secrets["DB_USERNAME"],
-)
+def fill_nan_with_zero(df):
+    return df.fillna(0)
 
 def main():
     st.title("FILL NaN with 0")
@@ -34,12 +24,22 @@ def main():
         st.dataframe(df)
 
         # Fill NaN values with 0
-        df_filled = df.fillna(0)
+        df_filled = fill_nan_with_zero(df)
 
         # Download button
         output_buffer = io.BytesIO()
         df_filled.to_excel(output_buffer, index=False)
         st.download_button("Download filled DataFrame as Excel", data=output_buffer.getvalue(), file_name="output.xlsx", mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
 
+    # Accessing secrets
+    st.write("DB username:", st.secrets["db_username"])
+    st.write("DB password:", st.secrets["db_password"])
+    st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+    # And the root-level secrets are also accessible as environment variables:
+    import os
+    st.write("Has environment variables been set:", os.environ["db_username"] == st.secrets["db_username"])
+
 if __name__ == "__main__":
     main()
+
